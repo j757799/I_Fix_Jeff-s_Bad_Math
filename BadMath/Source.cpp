01e7fea60b5f5a44ec6981a515cfdee2a5e22f15
JesseFixes <JesseFixes@jessefixes.com>
2021-03-19 14:34:59 -0400

    I finally found the fix

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,18 +2,15 @@
 #include <iomanip>
 using namespace std;
 
-float average(float i1, float i2, float i3)
-{
-	return (i1 + i2 + i3) / 3;
-}
+float average(float, float, float);
 
 int main()
 {
-	float n1 =0.0 , n2 = 0.0, n3 = 0.0;
+	float n1 =0.0, n2 = 0.0, n3 = 0.0;
 	cout << "This program calculates the average of three numbers." << endl << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
@@ -22,4 +19,12 @@ int main()
 	system("pause");
 
 	return 0;
+}
+
+float average(float i1, float i2, float i3)
+{
+	
+	 float average = ((i1 + i2 + i3) / (3));
+	
+	return average;
 }
